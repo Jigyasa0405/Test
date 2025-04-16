c31689e7614757271cc229157d0873e588b9e00c
Jigyasa Patra <patrajigyasa@gmail.com>
2025-04-16 20:02:17 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name= 'Generative MedBot',
+    version= '0.0.0',
+    author= 'Jigyasa Patra',
+    author_email= 'patrajigyasa@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+)
